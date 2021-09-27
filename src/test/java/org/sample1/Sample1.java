43d492adaa4df66d5bf7763a5565ff22541a8ae1
SakthikumarVR <saravanasakthi1820@gmail.com>
2021-09-27 14:14:24 +0530

    LoadURL

--- src/test/java/org/sample1/Sample1.java
+++ src/test/java/org/sample1/Sample1.java
@@ -1,11 +1,16 @@
 package org.sample1;
 
 public class Sample1 {
-	public static void password() {
-		System.out.println("PassWord");
+	public static void loadURL() {
+		
+		System.out.println("LoadURL");
+		
 	}
+	
 	public static void main(String[] args) {
-		password();
+	
+		loadURL();
+	
 	}
-
+	
 }
