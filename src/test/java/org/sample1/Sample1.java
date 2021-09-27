478060fb01ac2f30a05205561d6fa1a668ac0879
SakthikumarVR <saravanasakthi1820@gmail.com>
2021-09-27 14:07:20 +0530

    Login

--- src/test/java/org/sample1/Sample1.java
+++ src/test/java/org/sample1/Sample1.java
@@ -1,11 +1,11 @@
 package org.sample1;
 
 public class Sample1 {
-	public static void password() {
+	public static void passwrd() {
 		System.out.println("PassWord");
 	}
 	public static void main(String[] args) {
-		password();
+		passwrd();
 	}
 
 }
---
+++ src/test/java/org/sample2/Sample2.java
@@ -0,0 +1,11 @@
+package org.sample2;
+
+public class Sample2 {
+	public static void login() {
+		System.out.println("Login");
+	}
+	public static void main(String[] args) {
+		login();
+	}
+
+}
