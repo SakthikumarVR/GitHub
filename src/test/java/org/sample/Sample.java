ba38a7c5c8bd7f16e1e673270435bb031f5d7645
SakthikumarVR <saravanasakthi1820@gmail.com>
2021-09-27 13:49:21 +0530

    Master Task Initiated

---
+++ src/test/java/org/sample/Sample.java
@@ -0,0 +1,11 @@
+package org.sample;
+
+public class Sample {
+	public static void username() {
+		System.out.println("UserName");
+	}
+	public static void main(String[] args) {
+		username();
+	}
+
+}
---
+++ src/test/java/org/sample1/Sample1.java
@@ -0,0 +1,11 @@
+package org.sample1;
+
+public class Sample1 {
+	public static void password() {
+		System.out.println("PassWord");
+	}
+	public static void main(String[] args) {
+		password();
+	}
+
+}
